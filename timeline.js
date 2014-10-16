9bb44fe2ba625f993cbb6e62759de1f732375dce
Mickael Allonneau <mickael.allonneau@gmail.com>
2014-10-16 15:36:03 +0200

    timeline> Added 'addQuoteButton()' function

--- timeline.js
+++ timeline.js
@@ -1,29 +1,33 @@
 // Get all the "··· More" menus
 var menus = getMenusArray();
 
-for (var index in menus) {
-    var newItem = $(document.createElement('li'));
-    var innerButton = $(document.createElement('button'));
-    var menu = $(menus[index]);
-    var quoteButtonText = chrome.i18n.getMessage("quote");
-
-    // Set up the button
-    innerButton.attr('class', 'dropdown-link');
-    innerButton.attr('role', 'menuitem');
-    innerButton.attr('type', 'button');
-    // Attach it the click listener
-    innerButton.click(onClickQuoteButton);
-    // Set the button's text
-    innerButton.text(quoteButtonText);
-    // Add the "Quote" button
-    newItem.prepend(innerButton);
-    // Add the item to the list
-    menu.prepend(newItem);
-}
+addQuoteButton(menus);
 
 /**
  ** FUNCTIONS
  **/
+
+function addQuoteButton(menus) {
+    for (var index in menus) {
+        var newItem = $(document.createElement('li'));
+        var innerButton = $(document.createElement('button'));
+        var menu = $(menus[index]);
+        var quoteButtonText = chrome.i18n.getMessage("quote");
+
+        // Set up the button
+        innerButton.attr('class', 'dropdown-link');
+        innerButton.attr('role', 'menuitem');
+        innerButton.attr('type', 'button');
+        // Attach it the click listener
+        innerButton.click(onClickQuoteButton);
+        // Set the button's text
+        innerButton.text(quoteButtonText);
+        // Add the "Quote" button
+        newItem.prepend(innerButton);
+        // Add the item to the list
+        menu.prepend(newItem);
+    }
+}
 
 function getMenusArray() {
     return [].filter.call(
